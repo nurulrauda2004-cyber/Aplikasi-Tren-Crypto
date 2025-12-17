96b00672f9c9a2d94d0be891816a5e7477694fab
nurulrauda2004-cyber <nurulrauda2004@gmail.com>
2025-12-17 20:55:46 +0700

    Change data source to local CSV file
    Remove data loading from URL and read from local file instead.

--- app.py
+++ app.py
@@ -7,24 +7,16 @@ st.set_page_config(page_title="Aplikasi Tren Crypto")
 st.title("📈 Aplikasi Tren Crypto")
 st.caption("Melihat naik atau turunnya harga crypto")
 
-DATA_URL = "https://raw.githubusercontent.com/datasets/bitcoin-price/master/data/bitcoin.csv"
+# Baca data dari file lokal
+df = pd.read_csv("bitcoin.csv")
+df["Date"] = pd.to_datetime(df["Date"])
 
-@st.cache_data
-def load_data(url):
-    df = pd.read_csv(url)
-    df["Date"] = pd.to_datetime(df["Date"])
-    return df
-
-df = load_data(DATA_URL)
-
+# Tampilkan grafik
 fig, ax = plt.subplots()
 ax.plot(df["Date"], df["Price"])
 ax.set_xlabel("Tanggal")
 ax.set_ylabel("Harga")
 st.pyplot(fig)
 
-if df["Price"].iloc[-1] > df["Price"].iloc[0]:
-    st.success("📈 Tren Naik")
-else:
-    st.error("📉 Tren Turun")
-
+# Kesimpulan sederhana
+if df["Price"].iloc[-1] > df["Pri]()
