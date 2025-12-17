e20163102b5ddc6d5dfdf98bbd11fa70b63b18e9
nurulrauda2004-cyber <nurulrauda2004@gmail.com>
2025-12-17 20:59:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ st.set_page_config(page_title="Aplikasi Tren Crypto")
 st.title("📈 Aplikasi Tren Crypto")
 st.caption("Melihat naik atau turunnya harga crypto")
 
-# Baca data dari file lokal
 df = pd.read_csv("bitcoin.csv")
 df["Date"] = pd.to_datetime(df["Date"])
 
-# Tampilkan grafik
 fig, ax = plt.subplots()
 ax.plot(df["Date"], df["Price"])
 ax.set_xlabel("Tanggal")
 ax.set_ylabel("Harga")
 st.pyplot(fig)
 
-# Kesimpulan sederhana
-if df["Price"].iloc[-1] > df["Pri]()
+if df["Price"].iloc[-1] > df["Price"].iloc[0]:
+    st.success("📈 Tren Naik")
+else:
+    st.error("📉 Tren Turun")
